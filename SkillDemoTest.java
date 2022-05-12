7018961bb1a57b23855b4bf17cbaa9a6bb436342
Weiji Chen <wjchen@ucsd.edu>
2022-05-11 17:39:03 -0700

    push

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test 
+    public void subtract() {
+        assertEquals(4, SkillDemo.subtract(5, 2)); //this fails 
+    }
+    
+}
